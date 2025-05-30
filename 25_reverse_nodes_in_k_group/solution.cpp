4fbcd91f4d8347fd2c7bc091f123e09e0e08651d
adrianbalbs <adrianbalbs@hotmail.com>
2025-05-30 10:00:58 +1000

    add daily

---
+++ 25_reverse_nodes_in_k_group/solution.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *prev = new ListNode();
+        ListNode *ans = prev;
+        vector<ListNode*> stack;
+        while (head) {
+            stack.push_back(head);
+            if (stack.size() == k) {
+                while (!stack.empty()) {
+                    ListNode *item = stack.back();
+                    prev->next = new ListNode(item->val);
+                    stack.pop_back();
+                    prev = prev->next;
+                }
+            }
+            head = head->next;
+        }
+
+        for (auto &node : stack) {
+            prev->next = node;
+            prev = prev->next;
+        }
+        return ans->next;
+    }
+};
---
+++ 3373_maximize_the_number_of_target_nodes_after_connecting_trees_ii/solution.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        vector<int> even = build(edges1, 0);
+        vector<int> odd = build(edges2, 1);
+        int max_val = *max_element(odd.begin(), odd.end());
+        vector<int> res;
+        for (auto &v : even) {
+            res.push_back(v + max_val);
+        }
+        return res;
+    }
+
+    vector<int> build(vector<vector<int>> &edges, int even) {
+        int n = edges.size() + 1;
+        vector<vector<int>> graph(n);
+        for (auto &e : edges) {
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
+        }
+        vector<int> color(n, -1);
+        vector<int> color_count(2);
+        color[0] = even;
+        color_count[even]++;
+
+        queue<pair<int, int>> queue;
+        queue.push({0, even});
+        while (!queue.empty()) {
+            auto [node, c] = queue.front();
+            queue.pop();
+            for (auto next : graph[node]) {
+                if (color[next] == -1) {
+                    color[next] = c ^ 1;
+                    color_count[color[next]]++;
+                    queue.push({next, color[next]});
+                }
+            }
+        }
+        vector<int> res;
+        for (auto &c : color) {
+            res.push_back(color_count[c]);
+        }
+        return res;
+    }
+
+};
