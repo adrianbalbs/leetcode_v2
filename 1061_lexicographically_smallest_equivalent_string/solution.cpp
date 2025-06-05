87075a7ce605c872460b62c9ed7fbee2c68186ca
adrianbalbs <adrianbalbs@hotmail.com>
2025-06-06 09:58:08 +1000

    leetcode daily 1061

---
+++ 1061_lexicographically_smallest_equivalent_string/solution.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class DisjointSetUnion {
+    private:
+        vector<int> parents;
+    public:
+        DisjointSetUnion(int size): parents(size) {
+            for (int i = 0; i < size; i++) {
+                parents[i] = i;
+            }
+        }
+
+        int find(int x) {
+            return parents[x] == x ? x : (parents[x] = find(parents[x]));
+        }
+
+        bool unite(int x, int y) {
+            int x_root = find(x);
+            int y_root = find(y);
+            if (x_root == y_root) {
+                return false;
+            }
+
+            // if x_root is lexicographically smaller, it will be the parent
+            if (x_root > y_root) {
+                swap(x_root, y_root);
+            }
+            parents[y_root] = x_root;
+            return true;
+        }
+};
+
+
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        DisjointSetUnion dsu(26);
+        int n = s1.size();
+        for (int i = 0; i < n; i++) {
+            dsu.unite(s1[i] - 'a', s2[i] - 'a');
+        }
+        for (char &c : baseStr) {
+            c = char(dsu.find(c - 'a') + 'a');
+        }
+        return baseStr;
+    }
+};
