d45bcc4923b5248c4b25dce94a603581166272af
adrianbalbs <adrianbalbs@hotmail.com>
2025-06-08 11:18:36 +1000

    daily

---
+++ 2434_using_a_robot_to_print_the_lexicographically_smallest_string/solution.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string robotWithString(string s) {
+        int n = s.size();
+        vector<char> min_at_pos(n);
+        min_at_pos[n - 1] = s[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            min_at_pos[i] = min(s[i], min_at_pos[i + 1]);
+        }
+        vector<char> t;
+        string res;
+
+        for (int i = 0; i < n; ++i) {
+            t.push_back(s[i]);
+            while (!t.empty() && t.back() <= min_at_pos[i + 1]) {
+                res += t.back();
+                t.pop_back();
+            }
+        }
+        while (!t.empty()) {
+            res += t.back();
+            t.pop_back();
+        }
+        return res;
+    }
+};
+
---
+++ 386_lexicographical_numbers/solution.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        int curr = 1;
+        res.push_back(curr);
+        while (res.size() != n) {
+            if (curr * 10 <= n) {
+                curr *= 10;
+                res.push_back(curr);
+            } else if (curr % 10 != 9 and curr + 1 <= n) {
+                res.push_back(++curr);
+            } else {
+                while (curr % 10 == 9 or curr + 1 > n) {
+                    curr /= 10;
+                }
+                res.push_back(++curr);
+            }
+        }
+        return res;
+    }
+};
