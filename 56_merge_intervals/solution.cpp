7fc895f7f6b5fb9f129b97ee7cf09332d12f6e0b
adrianbalbs <adrianbalbs@hotmail.com>
2025-06-15 20:24:05 +1000

    add solns to merge intervals and validate bin search tree

---
+++ 56_merge_intervals/solution.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> merge(vector<vector<int>> &intervals) {
+    vector<vector<int>> res;
+    sort(intervals.begin(), intervals.end());
+    res.push_back(intervals.front());
+    for (size_t i = 1; i < intervals.size(); i++) {
+      if (intervals[i][0] <= res.back()[1]) {
+        res[res.size() - 1][1] = max(res[res.size() - 1][1], intervals[i][1]);
+      } else {
+        res.push_back(intervals[i]);
+      }
+    }
+    return res;
+  }
+};
